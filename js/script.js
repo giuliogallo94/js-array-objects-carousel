2d66eb2f64ae543b6d5414867bba02b584c4cdfe
giuliogallo94 <giuliogallo1994@gmail.com>
2023-11-07 15:45:11 +0100

    add: object in DOM

--- js/script.js
+++ js/script.js
@@ -25,3 +25,64 @@ const images = [
     text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
   },
 ];
+
+//Stampa delle immagini sul DOM
+const itemsElem = document.querySelector(".items");
+let imagesString = "";
+images.forEach((curImages) => {
+  let item = curImages.image;
+  let titleImage = curImages.title;
+  let textImage = curImages.text;
+  imagesString += `
+<div class="item">
+<img src="${item}" alt="">
+<div class="description">
+<h6>${titleImage}</h6>
+<p>${textImage}</p>
+</div>
+</div>`;
+});
+
+console.log(imagesString);
+
+itemsElem.innerHTML = itemsElem.innerHTML + imagesString;
+
+// Stampa delle miniature sul DOM
+const miniElem = document.querySelector(".miniatures");
+console.log(miniElem);
+let miniatureString = "";
+images.forEach((curImages) => {
+  let miniature = curImages.image;
+  miniatureString += `
+    <div class="miniature-item">
+        <img src="${miniature}" alt="">
+    </div>`;
+});
+console.log(miniatureString);
+
+miniElem.innerHTML += miniatureString;
+
+// EVENT
+let currentIndex = 0;
+const slideElems = document.querySelectorAll(".item");
+slideElems[currentIndex].classList.add("active");
+
+document.querySelector(".next").addEventListener("click", function () {
+  slideElems[currentIndex].classList.remove("active");
+  if (currentIndex < slideElems.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  slideElems[currentIndex].classList.add("active");
+});
+
+document.querySelector(".prev").addEventListener("click", function () {
+  slideElems[currentIndex].classList.remove("active");
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = slideElems.length - 1;
+  }
+  slideElems[currentIndex].classList.add("active");
+});
